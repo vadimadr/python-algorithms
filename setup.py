c16cfb78a4cd12bbe4f75275627ac3923cfc63c6
Vadim Andronov <vadimadr@gmail.com>
2017-02-24 17:08:48 +0300

    declare requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 import os
 from setuptools import Extension
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+import sys
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 # Change if you want to compile *.pyx sources to *.cpp
 USE_CYTHON = False
@@ -32,8 +50,31 @@ if USE_CYTHON:
 else:
     extensions = no_cythonize(extensions)
 
-setup(name='algorithm', version='0.1', packages=find_packages(exclude=['test']),
-      url='https://github.com/vadimadr/python-algorithms.py', license='MIT',
-      author='Vadim Andronov', author_email='vadimadr@gmail.com',
-      description='Implementation of some common algorithms',
-      ext_modules=extensions)
+requirements = [
+    'numpy==1.12.0',
+    'scipy==0.18.1',
+    'matplotlib==2.0.0'
+]
+
+test_requirements = [
+    'pytest==3.0.6',
+    'pytest-cov==2.4.0',
+    'pytest-mock==1.5.0',
+]
+
+setup(
+    name='algorithm',
+    version='0.1',
+    packages=find_packages(exclude=['test']),
+    url='https://github.com/vadimadr/python-algorithms.py',
+    license='MIT',
+    author='Vadim Andronov', author_email='vadimadr@gmail.com',
+    description='Implementation of some common algorithms',
+    ext_modules=extensions,
+    zip_safe=False,
+    package_data={'': ['LICENSE']},
+    install_requires=requirements,
+    tests_require=test_requirements,
+    test_suite='tests',
+    cmdclass={'test': PyTest},
+)
