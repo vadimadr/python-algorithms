06a9e161e6a24b716e5615d01d7abdcb5a532fc3
Vadim Andronov <vadimadr@gmail.com>
2017-06-11 16:27:18 +0300

    add heap data structure

---
+++ algorithms/structures/heap.py
@@ -0,0 +1,116 @@
+"""Binary heap data structure. Heap is the binary tree with the following
+properties:
+1. parent is not greater (min-heap) or not less (max-heap) than it's children.
+2. all levels (except of deepest) are fully filled
+3. leaves are filled left to right.
+
+Example of min-heap:
+
+                                   0
+
+                  1                                 2
+
+          3               4                5               6
+
+      7       8       9       10      11      12      13      14
+
+    15 16   17 18   19 20   21 22   23 24   25 26   27 28   29 30
+
+Heap is stored in array where a[0] is a root, and a[i*2 + 1] and a[i*2 + 2]
+are left and right children respectively. Binary Heaps are used to represent
+priority queues or to sort without additional memory.
+"""
+
+
+class BinaryHeap:
+    def __init__(self, data=None, maxheap=False):
+        self.maxheap = maxheap
+        if data:
+            self.data = data.copy()
+            self.n = len(data)
+            self.heapify()
+        else:
+            self.data = []
+            self.n = 0
+
+    def siftdown(self, i):
+        """move element with index i to the down to restore heap properties
+
+        All children of i already heaps. Place i at leaf and bubble it up
+        until heap property is restored. It is possible to implement this
+        procedure by swapping i with its child until heap property is restored
+        but comparisons are potentially expensive and siftdown is usually
+        called after heappop (i.e. for large element) so it make sense to
+        start comparisons from the bottom.
+
+        Complexity: O(log N)
+        """
+
+        a = self.data
+        n = self.n
+        item = a[i]
+        root = i
+        child = 2 * i + 1
+        while child < n:
+            # select smallest child
+            maxheap = self.maxheap
+            if child + 1 < n and (not maxheap and a[child + 1] < a[child]
+            or maxheap and a[child] < a[child + 1]):
+                child += 1
+            a[i] = a[child]
+            i = child
+            child = 2 * i + 1
+        a[i] = item
+        self.siftup(i, root)
+
+    def siftup(self, i, root=0):
+        """move element with index i to the root to restore heap properties
+
+        Bubble up element until heap property is restored.
+
+        Complexity: O(log N)
+        """
+        a = self.data
+        item = a[i]
+        while i > root:
+            parent = (i - 1) // 2
+            if not self.maxheap and item < a[parent] \
+                    or self.maxheap and a[parent] < item:
+                a[i] = a[parent]
+                i = parent
+            else:
+                break
+        a[i] = item
+
+    def heapify(self):
+        """Transform list to heap in O(N).
+
+        Leaves are already heaps so start restoring heap property from its
+        parents.
+        """
+        for i in reversed(range(self.n // 2)):
+            self.siftdown(i)
+
+    def push(self, item):
+        """Add new item in O(log N)"""
+        self.data.append(item)
+        self.n += 1
+        self.siftup(self.n - 1)
+
+    def pop(self):
+        """Pops minimum (maximum) element and restores heap property"""
+        item = self.data[0]
+        self.data[0] = self.data[self.n - 1]
+        self.n -= 1
+        self.siftdown(0)
+        return item
+
+    def replace(self, i, newval):
+        """Replaces element with index i and restores heap property"""
+        oldval = self.data[i]
+        self.data[i] = newval
+        if not self.maxheap and newval < oldval \
+                or self.maxheap and oldval < newval:
+            self.siftup(i)
+        else:
+            self.siftdown(i)
---
+++ tests/test_heap.py
@@ -0,0 +1,59 @@
+from hypothesis import given
+from hypothesis.strategies import integers, lists
+
+from algorithms.structures.heap import BinaryHeap
+
+
+def is_heap(heap):
+    n = heap.n
+    a = heap.data
+    maxheap = heap.maxheap
+
+    for i in range(n):
+        left_child = i * 2 + 1
+        if left_child < n and (not maxheap and a[left_child] < a[i] or
+                maxheap and a[i] < a[left_child]):
+            return False
+        if left_child + 1 < n and (not maxheap and (a[left_child + 1] < a[i])
+        or maxheap and a[i] < a[left_child + 1]):
+            return False
+    return True
+
+
+@given(lists(integers()))
+def test_heapify_min(data):
+    heap = BinaryHeap(data)
+    assert is_heap(heap)
+
+
+@given(lists(integers()))
+def test_heapify_max(data):
+    heap = BinaryHeap(data, maxheap=True)
+    assert is_heap(heap)
+
+
+@given(lists(integers(), 1))
+def test_heap_pop(data):
+    heap = BinaryHeap(data)
+    assert is_heap(heap)
+    data_min = min(data)
+    heap_pop = heap.pop()
+    assert is_heap(heap)
+    assert heap_pop == data_min
+
+
+@given(lists(integers(), 1))
+def test_heap_pop_max(data):
+    heap = BinaryHeap(data, maxheap=True)
+    data_max = max(data)
+    heap_pop = heap.pop()
+    assert is_heap(heap)
+    assert heap_pop == data_max
+
+
+@given(lists(integers(), 1), integers(0), integers())
+def test_heap_replace(data, i, newval):
+    heap = BinaryHeap(data)
+    i %= len(data)
+    heap.replace(i, newval)
+    assert is_heap(heap)
