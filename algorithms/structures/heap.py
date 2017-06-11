953af4884a7508ef992781e5b0f1da2a3619eef9
Vadim Andronov <vadimadr@gmail.com>
2017-06-11 18:07:49 +0300

    fix code style

--- algorithms/structures/heap.py
+++ algorithms/structures/heap.py
@@ -54,8 +54,8 @@ class BinaryHeap:
         while child < n:
             # select smallest child
             maxheap = self.maxheap
-            if child + 1 < n and (not maxheap and a[child + 1] < a[child]
-            or maxheap and a[child] < a[child + 1]):
+            if child + 1 < n and (not maxheap and a[child + 1] < a[child] or
+                    maxheap and a[child] < a[child + 1]):
                 child += 1
             a[i] = a[child]
             i = child
