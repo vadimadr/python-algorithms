7272217a5b4b7b32aed2dde9553b8cbb76f4a904
vadim <vadimadr@gmail.com>
2019-02-24 22:20:34 +0300

    Added sqrt decomposition.

---
+++ algorithms/structures/range_queries/sqrt_decomposition.py
@@ -0,0 +1,50 @@
+from operator import add
+
+import numpy as np
+
+
+class SQRTDecomposition:
+    """Divide input array into sub-arrays of size B = sqrt(n), pre-compute cumulative frequency in every block:
+    [0, B), [B, 2*B), ... Then part of the query can be answered by taking the value from pre-computed block.
+    Then complexity of the query will be: T(n) = 2*B + (n / B) = O(sqrt(n))
+    """
+
+    def __init__(self, arr, op=add, initial=0):
+        self.initial = initial
+        self.function = op
+        self.block_size = int(np.sqrt(len(arr))) + 1
+        self.blocks = np.full(self.block_size, initial)
+        self.arr = arr
+
+        for i in range(len(arr)):
+            self.blocks[i // self.block_size] = op(self.blocks[i // self.block_size], arr[i])
+
+    def query(self, l, r):
+        ans = self.initial
+
+        # while l < r:
+        #     if l % self.block_size == 0 and l + self.block_size - 1 < r:
+        #         ans = self.function(ans, self.blocks[l // self.block_size])
+        #         l += self.block_size
+        #     else:
+        #         ans = self.function(ans, self.arr[l])
+        #         l += 1
+        # return ans
+
+        start_block = l // self.block_size
+        end_block = (r - 1) // self.block_size
+        if start_block >= end_block:
+            for i in range(l, r):
+                ans = self.function(ans, self.arr[i])
+            return ans
+        for i in range(l, self.block_size * (start_block + 1)):
+            ans = self.function(ans, self.arr[i])
+        for i in range(start_block + 1, end_block):
+            ans = self.function(ans, self.blocks[i])
+        for i in range(self.block_size * end_block, r):
+            ans = self.function(ans, self.arr[i])
+        return ans
+
+    def update(self, i, x):
+        self.arr[i] = self.function(self.arr[i], x)
+        self.blocks[i // self.block_size] = self.function(self.blocks[i // self.block_size], x)
--- tests/test_queries.py
+++ tests/test_queries.py
@@ -2,6 +2,7 @@ from hypothesis import assume, given
 from hypothesis.strategies import composite, lists, integers, tuples, permutations
 
 from algorithms.structures.range_queries.fenwick_tree import FenwickTree
+from algorithms.structures.range_queries.sqrt_decomposition import SQRTDecomposition
 
 
 @composite
@@ -11,22 +12,41 @@ def shuffle(draw, data):
 
 
 @composite
-def interval_queries(draw):
+def intervals(draw, arr_size):
+    queries = draw(lists(integers(1, arr_size)))
+    return [('q', r) for r in queries]
+
+
+@composite
+def ranges(draw, arr_size):
+    idxs = integers(1, arr_size)
+    data = tuples(idxs, idxs)
+    queries = draw(lists(data))
+    return [('q', min(l, r), max(l, r)) for l, r in queries]
+
+
+@composite
+def updates(draw, arr_size, data_range):
+    idxs = integers(0, arr_size - 1)
+    updates = draw(lists(tuples(idxs, data_range)))
+    return [('u', i, x) for i, x in updates]
+
+
+@composite
+def range_queries(draw, queries=ranges, updates=updates):
     data_range = integers(-1000, 1000)
     arr = draw(lists(data_range))
     assume(len(arr) > 0)
 
-    upd_indexes = integers(0, len(arr) - 1)
-    q_indexes = integers(1, len(arr))
-    updates = draw(lists(tuples(upd_indexes, data_range)))
-    queries = draw(lists(q_indexes))
+    queries = draw(queries(len(arr)))
+    if updates:
+        queries += draw(updates(len(arr), data_range))
 
-    queries = [('q', x) for x in queries] + [('u', i, x) for i, x in updates]
     queries = draw(permutations(queries))
     return arr, queries
 
 
-@given(interval_queries())
+@given(range_queries(queries=intervals, updates=updates))
 def test_fenwick(data):
     arr, queries = data
     tree = FenwickTree(arr)
@@ -37,3 +57,19 @@ def test_fenwick(data):
         if q[0] == 'u':
             arr[q[1]] += q[2]
             tree.update(q[1], q[2])
+
+
+@given(range_queries(queries=ranges, updates=updates))
+def test_sqrt_decompoisition(data):
+    arr, queries = data
+    struct = SQRTDecomposition(arr)
+    arr = arr.copy()
+
+    for q in queries:
+        if q[0] == 'q':
+            _, l, r = q
+            assert sum(arr[l:r]) == struct.query(l, r)
+        if q[0] == 'u':
+            _, i, x = q
+            arr[i] += x
+            struct.update(i, x)
