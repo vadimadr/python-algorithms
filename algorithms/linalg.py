99b5b3d7b871bd0d86361e256da00335f62b2618
Vadim Andronov <vadimadr@gmail.com>
2021-11-06 21:08:30 +0300

    Implemented LUP decomposition

--- algorithms/linalg.py
+++ algorithms/linalg.py
@@ -11,3 +11,42 @@ def matrix_product(lhs, rhs):
             for k in range(Q):
                 res[j, k] += lhs[j, i] * rhs[i, k]
     return res
+
+
+def LUP_decomposition(A, tol=1e-6):
+    """Factorize matrix A = PLU
+    using Gaussian Elimination in O(n^3)
+    L - is lower triangular, U - upper triangular, and P - is permutation matrix.
+    """
+    A = A.copy()
+    n, m = A.shape
+    p = np.arange(n)
+    for jc in range(min(n, m)):
+        # select pivot: maximize |U_{j,j}|
+        max_j = jc
+        for ir in range(jc + 1, n):
+            if abs(A[p[ir], jc]) > abs(A[p[max_j], jc]):
+                max_j = ir
+
+        # swap jc and max_j rows
+        p[max_j], p[jc] = p[jc], p[max_j]
+
+        if abs(A[p[jc], jc]) < tol:
+            # skip current row
+            # matrix is degenerate!
+            continue
+
+        for ir in range(jc + 1, n):
+            A[p[ir], jc] = A[p[ir], jc] / A[p[jc], jc]
+            for k in range(jc + 1, m):
+                A[p[ir], k] -= A[p[ir], jc] * A[p[jc], k]
+
+    # extract L and U matrices (optional step: can use matrix A itself)
+    L, U = np.eye(n), np.zeros((n, m))
+    for i in range(n):
+        for j in range(min(i, m)):
+            L[i, j] = A[p[i], j]
+        for j in range(i, m):
+            U[i, j] = A[p[i], j]
+
+    return L, U, p
--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-from algorithms.linalg import matrix_product
+from algorithms.linalg import matrix_product, LUP_decomposition
+from hypothesis.strategies import composite, integers, one_of, floats
+from hypothesis.extra.numpy import arrays
+from hypothesis import given
 
 
 def test_matrix_product():
@@ -9,3 +12,55 @@ def test_matrix_product():
     c = a.dot(b)
     d = matrix_product(a, b)
     assert np.allclose(c, d)
+
+
+@composite
+def st_floats(draw, mod=100, eps=1e6):
+    """Floats with moderate values"""
+    x = draw(floats(allow_nan=False, max_value=mod, min_value=-mod, width=32))
+    if abs(x) < eps:
+        return 0
+    return x
+
+
+@composite
+def st_matrix(draw, maxn=30):
+    n, m = draw(integers(1, maxn)), draw(integers(1, maxn))
+
+    elements = one_of(
+        integers(min_value=100, max_value=100),
+        st_floats(),
+    )
+
+    A = draw(arrays(np.float32, shape=(n, m), elements=elements))
+    return A
+
+
+class TestLUP:
+    def check_lup(self, x):
+        x = np.array(x)
+        l, u, p = LUP_decomposition(x)
+        n, m = x.shape
+        assert l.shape == (n, n)
+        assert u.shape == (n, m)
+        np.testing.assert_allclose(l @ u, x[p], atol=1e-4)
+
+        # check L is lower triangular
+        # check U is upper triangular
+
+    def test_gauss(self):
+        # must work with every permutation of rows
+        self.check_lup([[0.0], [0.0], [0.0]])
+        self.check_lup([[1, 2, 3], [7.0, 8, 9], [4, 5, 6]])
+        self.check_lup([[1, 2, 3], [4, 5, 6], [7.0, 8, 9]])
+        self.check_lup([[4, 5, 6], [7.0, 8, 9], [1, 2, 3]])
+        self.check_lup([[4, 5, 6], [1, 2, 3], [7.0, 8, 9]])
+        self.check_lup([[7.0, 8, 9], [4, 5, 6], [1, 2, 3]])
+        self.check_lup([[7.0, 8, 9], [1, 2, 3], [4, 5, 6]])
+        self.check_lup(np.array([[1, 2], [3, 4]], np.float32))
+        self.check_lup([[1.0], [1.0]])
+        self.check_lup([[1.0, 1.0]])
+
+    @given(st_matrix())
+    def test_lup(self, x):
+        self.check_lup(x)
